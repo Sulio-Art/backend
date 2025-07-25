a354862fca47ea47d02dfcbd63cbc8455daf9528
Onyx48 <onyxdev48@gmail.com>
2025-07-25 17:24:13 +0530

    Update auth.Routes.js

--- route/auth.Routes.js
+++ route/auth.Routes.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import {
   register,
@@ -9,21 +10,28 @@ import {
 } from "../controller/auth.Controlller.js";
 import {
   connectInstagramAccount,
-  getInstagramProfile,
+  getInstagramProfile, 
 } from "../controller/instagram.controller.js";
 import { protect } from "../middleware/auth.middleware.js";
+import { verifyUserOtp } from "../controller/verifyOtp.controller.js";
 
 const router = express.Router();
 
+
 router.post("/register", register);
+router.post("/verify-otp", verifyUserOtp);
+
 
 router.post("/login", login);
-router.post("/request-password-reset", requestPasswordReset);
-router.post("/reset-password", resetPassword);
 router.post("/logout", logout);
 router.get("/me", protect, getMe);
 
+
+router.post("/request-password-reset", requestPasswordReset);
+router.post("/reset-password", resetPassword);
+
+
 router.post("/instagram/connect", protect, connectInstagramAccount);
-router.get("/instagram/profile", protect, getInstagramProfile);
+router.get("/instagram/profile", protect, getInstagramProfile); 
 
 export default router;
